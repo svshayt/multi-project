23c30286f9aba6e063ba37a65dd52e0ee1298201
Vasily Shaytan <svshayt@gmail.com>
2025-01-02 10:46:01 +0300

    feat: Добавить демо LocalTime, LocalDate, LocalDateTime классов

--- knowledge-base/src/main/java/com/svshayt/demo/JavaTimePackageDemo.java
+++ knowledge-base/src/main/java/com/svshayt/demo/JavaTimePackageDemo.java
@@ -2,8 +2,12 @@ package com.svshayt.demo;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 
 public class JavaTimePackageDemo {
@@ -11,6 +15,9 @@ public class JavaTimePackageDemo {
     public static void main(String[] args) {
         durationDemo();
         instantDemo();
+        localTimeDemo();
+        localDateDemo();
+        localDateTimeDemo();
     }
 
     public static void durationDemo() {
@@ -60,4 +67,66 @@ public class JavaTimePackageDemo {
     }
 
 
+    public static void localTimeDemo() {
+        LocalTime now = LocalTime.now(); // текущее время
+        LocalTime specificTime = LocalTime.of(14, 30); // 14:30
+
+        System.out.println(now);
+        System.out.println(specificTime);
+    }
+
+    public static void localDateDemo() {
+        LocalDate today = LocalDate.now(); // текущая дата
+        LocalDate specificDate = LocalDate.of(2021, Month.JANUARY, 1); // 1 января 2021 года
+
+        System.out.println(today);
+        System.out.println(specificDate);
+
+        //Сравнение дат
+        LocalDate date1 = LocalDate.of(2021, Month.JANUARY, 1);
+        LocalDate date2 = LocalDate.of(2022, Month.JANUARY, 1);
+
+        boolean isBefore = date1.isBefore(date2); // true
+        boolean isAfter = date1.isAfter(date2);   // false
+
+        System.out.println(isBefore);
+        System.out.println(isAfter);
+
+        //Добавление и вычитание времени
+        LocalDate today2 = LocalDate.now();
+        LocalDate nextWeek = today2.plusWeeks(1); // добавляем неделю
+        LocalDate lastMonth = today2.minusMonths(1); // вычитаем месяц
+
+        System.out.println(today2);
+        System.out.println(nextWeek);
+        System.out.println(lastMonth);
+
+
+        System.out.println("printDemoLocalDate_START");
+        var now = LocalDate.now();
+        System.out.println(now); // 2024-12-10
+        System.out.println(now.atStartOfDay()); // 2024-12-10T00:00
+        System.out.println("printDemoLocalDate_END");
+
+        System.out.println("printMethodOfLocalDate_START");
+        var now2 = LocalDate.now();
+        System.out.println(now2); // 2024-12-10
+        System.out.println(now2.atStartOfDay()); // 2024-12-10T00:00
+        System.out.println("printMethodOfLocalDate_END");
+    }
+
+    public static void localDateTimeDemo() {
+        LocalDateTime now = LocalDateTime.now(); // текущая дата и время
+        LocalDateTime specificDateTime = LocalDateTime.of(2021, Month.JANUARY, 1, 14, 30); // 1 января 2021 года, 14:30
+
+        System.out.println(now);
+        System.out.println(specificDateTime);
+
+        //Конвертация LocalDateTime в Instant
+        LocalDateTime localDateTime = LocalDateTime.now();
+        Instant instant = localDateTime.toInstant(ZoneOffset.UTC);
+
+        System.out.println(localDateTime);
+        System.out.println(instant);
+    }
 }
