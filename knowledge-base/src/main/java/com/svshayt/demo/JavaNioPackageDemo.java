69d7a67a9ac31a76523f3f5fa92f1d1b86aee197
Vasily Shaytan <svshayt@gmail.com>
2025-01-03 00:05:19 +0300

    feat: Добавить демо StandardCharsets класса

---
+++ knowledge-base/src/main/java/com/svshayt/demo/JavaNioPackageDemo.java
@@ -0,0 +1,15 @@
+package com.svshayt.demo;
+
+import java.nio.charset.StandardCharsets;
+
+public class JavaNioPackageDemo {
+
+    public static void main(String[] args) {
+        standardCharsetsDemo();
+    }
+
+    public static void standardCharsetsDemo() {
+        var charset = StandardCharsets.UTF_8;
+        System.out.println(charset);
+    }
+}
