c1f8c2b8a5b0030bb42182681de1c0017caba7ae
Vasily Shaytan <svshayt@gmail.com>
2025-01-06 10:16:47 +0300

    feat: добавить демо для BufferedReader, InputStream, InputStreamReader классов

--- knowledge-base/src/main/java/com/svshayt/demo/JavaNioPackageDemo.java
+++ knowledge-base/src/main/java/com/svshayt/demo/JavaNioPackageDemo.java
@@ -1,15 +1,52 @@
 package com.svshayt.demo;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class JavaNioPackageDemo {
 
     public static void main(String[] args) {
         standardCharsetsDemo();
+        bufferedReaderDemo();
+        bufferedReader2Demo();
     }
 
     public static void standardCharsetsDemo() {
         var charset = StandardCharsets.UTF_8;
         System.out.println(charset);
+    }
+
+    public static void bufferedReaderDemo() {
+        try {
+            String path = "demo/large_text.txt";
+            InputStream inputStream = Thread.currentThread().getContextClassLoader()
+                    .getResourceAsStream(path);
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(Objects.requireNonNull(inputStream), StandardCharsets.UTF_8)
+            );
+            reader.lines().forEach(System.out::println);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static void bufferedReader2Demo() {
+        try {
+            String path = "demo/small_text.txt";
+            InputStream inputStream = Thread.currentThread().getContextClassLoader()
+                    .getResourceAsStream(path);
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(Objects.requireNonNull(inputStream), StandardCharsets.UTF_8)
+            );
+            var content = reader.lines().collect(Collectors.joining("\n"));
+            System.out.println(content);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
