17df518738df8efe563994a89471bd9d6e15fab2
Vasily Shaytan <svshayt@gmail.com>
2025-01-07 13:12:07 +0300

    feat: добавить класс Person для тестирования методов класса Object

--- knowledge-base/src/main/java/com/svshayt/demo/JavaUtilPackageDemo.java
+++ knowledge-base/src/main/java/com/svshayt/demo/JavaUtilPackageDemo.java
@@ -1,11 +1,14 @@
 package com.svshayt.demo;
 
+import com.svshayt.model.Person;
+
 import java.util.ArrayList;
 
 public class JavaUtilPackageDemo {
 
     public static void main(String[] args) {
         listDemo();
+        objectDemo();
     }
 
     public static void listDemo() {
@@ -15,5 +18,22 @@ public class JavaUtilPackageDemo {
         list.add("C");
         System.out.println(list);
         System.out.println(list.getFirst());
+    }
+
+    public static void objectDemo() {
+        var person = Person.builder()
+                .firstName("John")
+                .lastName("Doe")
+                .build();
+        var personTheSame = Person.builder()
+                .firstName("John")
+                .lastName("Doe")
+                .build();
+
+        System.out.println(person.toString()); // Person(firstName=John, lastName=Doe, age=null, email=null, phone=null, address=null, city=null, country=null, birthDate=null, birthPlace=null)
+        System.out.println(person.equals(personTheSame)); // false
+        System.out.println(person.hashCode()); // 1599771323
+        System.out.println(person.getClass()); // class com.svshayt.model.Person
+
     }
 }
---
+++ knowledge-base/src/main/java/com/svshayt/model/Person.java
@@ -0,0 +1,28 @@
+package com.svshayt.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@RequiredArgsConstructor
+public class Person {
+    private final String firstName;
+    private final String lastName;
+    private final Integer age;
+    private final String email;
+    private final String phone;
+    private final String address;
+    private final String city;
+    private final String country;
+    private final LocalDate birthDate;
+    private final String birthPlace;
+}
+
